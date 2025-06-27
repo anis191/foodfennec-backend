dba4252e1ea521550b9e3454b9c0866bb6bc9fee
anisul alam <anisulalam2002@gmail.com>
2025-06-27 20:32:56 +0600

    user model customize

--- foodfennec/settings.py
+++ foodfennec/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+AUTH_USER_MODEL = 'users.User'
 
 # Application definition
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from users.managers import CustomUserManager
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    address = models.TextField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    RESTAURANT_OWNER = 'Restaurant_Owner'
+    CUSTOMER = 'Customer'
+    DELIVERY_MAN = 'Delivery_Man'
+    ROLE_CHOICES = [
+        (RESTAURANT_OWNER, 'Restaurant_Owner'),
+        (CUSTOMER, 'Customer'),
+        (DELIVERY_MAN, 'Delivery_Man'),
+    ]
+    role = models.CharField(max_length=20,choices=ROLE_CHOICES,null=True,blank=True,default=None)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
